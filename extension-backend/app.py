f05256950b9260559d285e74608b9d88575ac61f
sanjana kothari <sanjanak98@gmail.com>
2024-03-09 22:44:51 -0800

    server-ui integration

--- extension-backend/app.py
+++ extension-backend/app.py
@@ -1,21 +1,39 @@
-from src.realtime_audio import RealtimeAudio
+from init import initialise_objects
+# from src.realtime_audio import RealtimeAudio
 from src.user_interaction import UserInteraction
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def meettrack_main():
-    video_file = "" 
-    audio_file = ""
-    RealtimeAudio(video_file, audio_file).realtime_audio_pipeline()
+# Initialize objects within the Flask application context
+with app.app_context():
+  initialise_objects()
 
-    # get nearest segments for a query
-    query = ""
-    response = UserInteraction(query).query_response_pipeline()
+@app.route("/details", methods=["POST"])
+def meeting_details():
+  name = request.json.get("name")
+  date = request.json.get("date")
+  app.config["init_obj"].MEETING_NAME = name
+  app.config["init_obj"].MEETING_DATE = date
+  return jsonify()
 
-    return response
+@app.route("/record", methods=["POST"])
+def record():
+  # video_file = "" 
+  # audio_file = ""
+  # RealtimeAudio(video_file, audio_file).realtime_audio_pipeline()
+  pass
+
+@app.route("/query", methods=["POST"])
+def query():
+  query = request.json.get("userInput")
+  response = UserInteraction(query).query_response_pipeline()
+  data = {"response": response}
+  return jsonify(data)
+  
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
