8ccc6d50bcff32388c44f764f157a191a089cf7b
sanjana kothari <sanjanak98@gmail.com>
2024-03-17 14:17:51 -0700

    ui changes for multiple users

--- extension-frontend/extension.js
+++ extension-frontend/extension.js
@@ -7,12 +7,20 @@ let media = null;
 
 document.addEventListener("DOMContentLoaded", function () {
   var meetingDetailsForm = document.getElementById("meetingDetailsForm");
-  var toggleButton = document.getElementById("toggleButton");
+  var meetingIDForm = document.querySelector('.meetingIDForm');
+  var startRecordingButton = document.getElementById("startRecordingButton");
+  var stopRecordingButton = document.getElementById("stopRecordingButton");
   var userInputForm = document.getElementById("userInputForm");
-  
-  toggleButton.addEventListener("click", function () {
+   
+  startRecordingButton.addEventListener("click", function () {
     chrome.runtime.sendMessage({
-      type: "toggle-recording",
+      type: "start-recording",
+    });
+  });
+
+  stopRecordingButton.addEventListener("click", function () {
+    chrome.runtime.sendMessage({
+      type: "stop-recording",
     });
   });
 
@@ -21,25 +29,62 @@ document.addEventListener("DOMContentLoaded", function () {
     var name = document.getElementById('meetingName').value;
     var meetingType = document.querySelector('input[name="meetingType"]:checked').value;
 
-    fetch("http://localhost:5000/meeting_details", {
+    fetch("http://localhost:5000/submit-meeting-details", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ name: name, meetingType: meetingType})
     })
+    .then(response => response.json())
+    .then(data => {
+      if (data.meeting_id) {
+        var confirmation = confirm("Meeting ID: " + data.meeting_id + "\nClick OK to start the meeting session.");
+      } else {
+        var confirmation = confirm(data.error);
+      } 
+
+      if (confirmation) {
+        startMeetingSession()
+      }
+    })
     .catch(error => console.error("Error:", error));
 
     meetingDetailsForm.classList.add("disabled");
-    toggleButton.classList.remove("disabled");
+    startRecordingButton.classList.remove("disabled");
+    stopRecordingButton.classList.remove("disabled");
     userInputForm.classList.remove("disabled");
   });
+
+  meetingIDForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var meeting_id = document.getElementById('meetingID').value;
+
+    fetch("http://localhost:5000/join-meeting-session", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ meeting_id: meeting_id})
+    })
+    .then(response => response.json())
+    .then(data => {
+    })
+    .catch(error => console.error("Error:", error));
+
+    meetingDetailsForm.classList.add("disabled");
+    startRecordingButton.classList.remove("disabled");
+    stopRecordingButton.classList.remove("disabled");
+    userInputForm.classList.remove("disabled");
+    meetingIDForm.classList.add("disabled");
+  });
+
 
   userInputForm.addEventListener("submit", function (event) {
     event.preventDefault();
     var userInput = document.getElementById('userInput').value;
 
-    fetch("http://localhost:5000/user_query", {
+    fetch("http://localhost:5000/answer-user-query", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -49,10 +94,25 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(response => response.json())
     .then(data => {
       textDisplay.textContent = data.response;
-  })
+    })
     .catch(error => console.error("Error:", error));
   });
 });
+
+function startMeetingSession() {
+  // Make a request to the Flask server to start the meeting session
+  fetch("http://localhost:5000/start-meeting-session", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ /* Include any necessary data */ })
+  })
+  .then(response => {
+  })
+  .catch(error => console.error("Error:", error));
+}
+
 
 chrome.runtime.onMessage.addListener(async (message) => {
   switch (message.type) {
@@ -69,73 +129,99 @@ chrome.runtime.onMessage.addListener(async (message) => {
 
 async function startRecording(streamId) {
   try {
-    // Check if a recording is already in progress
+    // Check if a recording is already in progress - for the user who starts the recording but we are disbaling the button so may be redundant
     if (recorder && recorder.state === "recording") {
+      alert("Recording is already in progress.");
       return;
     }
 
-    // Create media stream for the first time
-    try{
-      if (mediaStreamFlag) {
-        media = await navigator.mediaDevices.getUserMedia({
-          audio: {
-            mandatory: {
-              chromeMediaSource: "tab",
-              chromeMediaSourceId: streamId
+    // Check if the meeting is already being recorded and prevent uother users from satrting a new recording
+    const recordingStatusResponse = await fetch("http://localhost:5000/meeting-recording-status", {
+      method: "POST"
+    });
+
+    if (recordingStatusResponse.status === 200) {
+      // Create media stream for the first time
+      try{
+        if (mediaStreamFlag) {
+          media = await navigator.mediaDevices.getUserMedia({
+            audio: {
+              mandatory: {
+                chromeMediaSource: "tab",
+                chromeMediaSourceId: streamId
+              }
+            },
+            video: {
+              mandatory: {
+                chromeMediaSource: "tab",
+                chromeMediaSourceId: streamId
+              }
             }
-          },
-          video: {
-            mandatory: {
-              chromeMediaSource: "tab",
-              chromeMediaSourceId: streamId
+          });
+
+          const output = new AudioContext();
+          const source = output.createMediaStreamSource(media);
+          source.connect(output.destination);
+
+          mediaStreamFlag = false;
+        }
+      } catch (error) {
+        console.error("Error starting tab capture:", error);
+      };
+
+      recorder = new MediaRecorder(media, { mimeType: "video/webm" });
+
+      recorder.ondataavailable = (event) => {
+        data.push(event.data);
+      }
+
+      recorder.onstop = async () => {
+        const blob = new Blob(data, { type: "video/webm" });
+        const recordingUrl = URL.createObjectURL(blob);
+
+        try {
+          const formData = new FormData();
+          formData.append("recording", blob);
+
+          await fetch("http://localhost:5000/process-meeting-recording", {
+              method: "POST",
+              body: formData
+          })
+          .then(response => {
+            if (response.status === 200) {
+                console.log("Meeting recording processed successfully.");
+            } else
+            if (response.status === 400) {
+                alert("Error in processing meeting recording.");
+            } else {
+                throw new Error("Unexpected response status: " + response.status);
             }
-          }
-        });
+          });
+        } catch (error) {
+            console.error("Error in recording:", error);
+        }
 
-        const output = new AudioContext();
-        const source = output.createMediaStreamSource(media);
-        source.connect(output.destination);
+        URL.revokeObjectURL(recordingUrl);
 
-        mediaStreamFlag = false;
-    }
-  } catch (error) {
-    console.error("Error starting tab capture:", error);
-  };
+        // Clear state ready for next recording
+        data = [];
 
-    recorder = new MediaRecorder(media, { mimeType: "video/webm" });
+        // Start the next recording after a 1 millisecond delay
+        setTimeout(() => startRecording(streamId), 1);
+      };
 
-    recorder.ondataavailable = (event) => {
-      data.push(event.data);
-    }
-
-    recorder.onstop = () => {
-      const blob = new Blob(data, { type: "video/webm" });
-      const recordingUrl = URL.createObjectURL(blob);
-
-      // CHANGE TO UPLOAD TO S3 BUCKET AND MAINTAIN SEQUENTIAL ORDERING OF FILE NAMES
-      const a = document.createElement("a");
-      a.style.display = "none";
-      document.body.appendChild(a);
-      a.href = recordingUrl;
-      a.download = "recording.webm";
-      a.click();
-
-      URL.revokeObjectURL(recordingUrl);
-
-      // Clear state ready for next recording
-      data = [];
-
-      // Start the next recording after a 1 millisecond delay
-      setTimeout(() => startRecording(streamId), 1);
-    };
-
-    // Start the recorder
-    if (recorderStateFlag) {
-      recorder.start();
-      // Stop the recorder after 30 seconds
-      stopTimer = setTimeout(() => {
-        stopRecording();
-      }, 30000);
+      // Start the recorder
+      if (recorderStateFlag) {
+        recorder.start();
+        // Stop the recorder after 30 seconds
+        stopTimer = setTimeout(() => {
+          stopRecording();
+        }, 30000);
+      }
+    } else if (recordingStatusResponse.status === 400) {
+      alert("Recording is already in progress.");
+    } else {
+      throw new Error("Unexpected response status: " + recordingStatusResponse.status);
     }
   } catch (error) {
     console.error("Error starting tab capture:", error);
@@ -150,6 +236,7 @@ function stopRecording() {
     {
       recorder.stream.getTracks().forEach((t) => t.stop());
       recorderStateFlag = false;
+      alert("Recording stopped");
     } 
   }
 }
--- extension-frontend/service-worker.js
+++ extension-frontend/service-worker.js
@@ -1,45 +1,64 @@
 let recording = false;
 
 chrome.runtime.onMessage.addListener(async (message) => {
-  if (message.type === "toggle-recording") {
-    const existingContexts = await chrome.runtime.getContexts({});
-    const offscreenDocument = existingContexts.find(
-      (c) => c.contextType === "OFFSCREEN_DOCUMENT"
-    );
-
-    // If an offscreen document is not already open, create one.
-    if (!offscreenDocument) {
-      // Create an offscreen document.
-      await chrome.offscreen.createDocument({
-        url: "extension.html",
-        reasons: ["USER_MEDIA"],
-        justification: "Recording from chrome.tabCapture API"
-      });
-    }
-    toggleRecording();
+  if (message.type === "start-recording") {
+    createOffScreenDocument();
+    sendStartMessage();
+  }
+  else if (message.type === "stop-recording") {
+    sendStopMessage();
   }
 });
 
-async function toggleRecording() {
-  if (recording) {
-    chrome.runtime.sendMessage({
-      type: "stop-recording",
+async function createOffScreenDocument() {
+  const existingContexts = await chrome.runtime.getContexts({});
+  const offscreenDocument = existingContexts.find(
+    (c) => c.contextType === "OFFSCREEN_DOCUMENT"
+  );
+
+  // If an offscreen document is not already open, create one.
+  if (!offscreenDocument) {
+    // Create an offscreen document.
+    await chrome.offscreen.createDocument({
+      url: "extension.html",
+      reasons: ["USER_MEDIA"],
+      justification: "Recording from chrome.tabCapture API"
     });
-    recording = false;
-  } 
-  else {
-    // Get a MediaStream for the active tab.
-    const tab = await chrome.tabs.query({ active: true, currentWindow: true });
-    const streamId = await chrome.tabCapture.getMediaStreamId({
-      targetTabId: tab[0].id
-    });
+  }
+}
+
+
+async function getMediaStream() {
+  // Get a MediaStream for the active tab.
+  const tab = await chrome.tabs.query({ active: true, currentWindow: true });
+  return await chrome.tabCapture.getMediaStreamId({
+    targetTabId: tab[0].id
+  });
+}
+
+
+async function sendStartMessage() {
+  try {
+    const streamId = await getMediaStream();
 
     chrome.runtime.sendMessage({
       type: "start-recording",
       data: streamId
     });
     recording = true;
+  } catch (error) {
+    console.error("Error getting media stream:", error);
   }
+}
+
+
+function sendStopMessage() {
+  if (recording) {
+    chrome.runtime.sendMessage({
+      type: "stop-recording",
+    });
+    recording = false;
+  } 
 }
 
 
